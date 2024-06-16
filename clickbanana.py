01f5bef28d578f81af9ef0e8bf554d47007b7992
0xGingi <104647854+0xGingi@users.noreply.github.com>
2024-06-16 11:52:16 -0400

    Add Prints on Start/Stop

--- clickbanana.py
+++ clickbanana.py
@@ -7,12 +7,19 @@ running = False
 
 def toggle_running(e):
     global running
+    if running:
+        print("Stopped clicking banana")
     running = not running
+    if running:
+        print("Gonna start clicking banana")
 
 keyboard.on_press_key("]", toggle_running, suppress=True)
+
+print("Script started! Press ] to start and stop")
 
 while True:
     if running:
         dlg = app.window(title='Banana')
         dlg.click_input(coords=(320, 320))
+        print("Clicked Banana")
         time.sleep(3600)
